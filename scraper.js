93c17fd76eb033da3081f32e3cb390ba0e6bb062
MIchael Huang <michaelnctu@gmail.com>
2020-11-06 19:20:07 +0800

    runTime/rating/title/imdbID done

--- scraper.js
+++ scraper.js
@@ -1,8 +1,20 @@
+//=>  <ul id="fruits">
+//      <li class="apple">Apple</li>
+//      <li class="orange">Orange</li>
+//      <li class="pear">Pear</li>
+//    </ul>
+
 
 const fetch = require('node-fetch');
 const cheerio = require('cheerio');
+const { response } = require('express');
 
 const searchUrl = 'https://www.imdb.com/find?q=';
+const movieUrl = 'https://www.imdb.com/title/'
+
+
+
+
 
 const searchCache = {};
 
@@ -31,10 +43,57 @@ function searchMovies(searchTerm) {
     });
 }
 
+function getMovie(imdbID) {
+
+
+  return fetch(`${movieUrl}${imdbID}`)
+    .then(response => response.text())
+    .then(body => {
+      const $ = cheerio.load(body);
+      const $title = $('.title_wrapper h1');
+
+      const imdbID = $title.attr('href').match(/title\/(.*)\//)[1]
+      const title = $title.first().contents().filter(function () {  //first()第一個元素
+        return this.type === 'text';
+      }).text().trim();
+      const rating = $('span[itemprop="ratingValue"]').text();
+      const runTime = $('time').first().contents().text().trim()
+
+      $('meta[itemProp="contentRating"]').attr('content');
+
+
+      const movie = {
+        imdbID,
+        title,
+        rating,
+        runTime,
+        // genres,
+        // datePublished,
+        // imdbRating,
+        // poster,
+        // summary,
+        // directors,
+        // writers,
+        // stars,
+        // storyLine,
+        // companies,
+        // trailer: `https://www.imdb.com${trailer}`
+      };
+
+      // movieCache[imdbID] = movie;
+
+      console.log(movie)
+
+      return movie;
+    });
+}
+
+
 
 
 module.exports = {
-  searchMovies
+  searchMovies,
+  getMovie
 };
 
 
