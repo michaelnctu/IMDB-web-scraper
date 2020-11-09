9517dc01ee241787123d43c41beca31e84a37684
MIchael Huang <michaelnctu@gmail.com>
2020-11-09 16:25:21 +0800

    debug done

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 const express = require('express')
 
 const scraper = require('./scraper')
-const cors = require('cors')
+
 
 const app = express()
 
-app.use(cors)
+
 
 app.get('/', (req, res) => {
   res.json({
